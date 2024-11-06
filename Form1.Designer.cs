45cfb7bf6c90bfc8c17aa32bf53b4db8a2a5a79b
ark-l <ark-l@ukr.net>
2024-11-06 21:44:39 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.tbN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDraw = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(824, 581);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(1054, 47);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(79, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Рандом";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(869, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(82, 20);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Відрізки";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(869, 56);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(115, 20);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Прямокутник";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // tbN
+            // 
+            this.tbN.Location = new System.Drawing.Point(1054, 12);
+            this.tbN.Name = "tbN";
+            this.tbN.Size = new System.Drawing.Size(100, 22);
+            this.tbN.TabIndex = 5;
+            this.tbN.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1005, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "N =";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(1190, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(115, 48);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(869, 119);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(115, 48);
+            this.btnDraw.TabIndex = 8;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1417, 605);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbN);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox tbN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Legends.Clear();
+            chart1.ChartAreas[0].AxisX.Enabled = AxisEnabled.False;
+            chart1.ChartAreas[0].AxisY.Enabled = AxisEnabled.False;
+            chart1.Series[0].ToolTip = "X = #VALX, Y = #VALY";
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = 30;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = 30;
+            radioButton1.Checked = true;
+        }
+        List<double> X = new List<double>();
+        List<double> Y = new List<double>();
+        List<double> X2 = new List<double>();
+        List<double> Y2 = new List<double>();
+        int c = 0;
+        private void chart1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                Series s;
+                X.Add(chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X));
+                Y.Add(chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Y));
+                if (c == 1)
+                {
+                    s = new Series();
+                    s.ChartType = SeriesChartType.Line;
+                    s.BorderWidth = 4;
+                    s.Points.AddXY(X[X.Count - 2], Y[Y.Count - 2]);
+                    s.Points.AddXY(X[X.Count - 1], Y[Y.Count - 1]);
+                    chart1.Series.Add(s);
+                    c = -1;
+                }
+                c++;
+            }
+            else if (radioButton2.Checked)
+            {
+                X2.Add(chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X));
+                Y2.Add(chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Y));
+                if (c == 1)
+                {
+                    chart1.Series[0].BorderWidth = 4;
+                    chart1.Series[0].Points.AddXY(X2[X2.Count - 2], Y2[Y2.Count - 2]);
+                    chart1.Series[0].Points.AddXY(X2[X2.Count - 2], Y2[Y2.Count - 1]);
+                    chart1.Series[0].Points.AddXY(X2[X2.Count - 1], Y2[Y2.Count - 1]);
+                    chart1.Series[0].Points.AddXY(X2[X2.Count - 1], Y2[Y2.Count - 2]);
+                    chart1.Series[0].Points.AddXY(X2[X2.Count - 2], Y2[Y2.Count - 2]);
+                    c = -1;
+                }
+                c++;
+            }
+            
+        }
+        void R()
+        {
+            Random r = new Random();
+            int x = r.Next(0, 30);
+            int y = r.Next(0, 30);
+            int x2 = r.Next(0, 30);
+            int y2 = r.Next(0, 30);
+            X2.Add(x);
+            X2.Add(x2);
+            Y2.Add(y);
+            Y2.Add(y2);
+            Series s = new Series();
+            s.ChartType = SeriesChartType.Line;
+            s.BorderWidth = 4;
+            s.Points.AddXY(x, y);
+            s.Points.AddXY(x2, y);
+            s.Points.AddXY(x2, y2);
+            s.Points.AddXY(x, y2);
+            s.Points.AddXY(x, y);
+            chart1.Series.Add(s);
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked && radioButton1.Checked)
+            {
+                Random r = new Random();
+                int N = Convert.ToInt32(tbN.Text);
+                for(int i = 0; i < N; i++)
+                {
+                    int x = r.Next(0, 30);
+                    int y = r.Next(0, 30);
+                    int x2 = r.Next(0, 30);
+                    int y2 = r.Next(0, 30);
+                    X.Add(x);
+                    X.Add(x2);
+                    Y.Add(y);
+                    Y.Add(y2);
+                    Series s = new Series();
+                    s.ChartType = SeriesChartType.Line;
+                    s.BorderWidth = 4;
+                    s.Points.AddXY(x, y);
+                    s.Points.AddXY(x2, y2);
+                    chart1.Series.Add(s);
+                    checkBox1.Checked = false;
+                }
+            }
+            else if(checkBox1.Checked && radioButton2.Checked)
+            {
+                R();
+                checkBox1.Checked = false;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < chart1.Series.Count; i++)
+            {
+                chart1.Series.RemoveAt(i);
+                i--;
+            }
+            chart1.Series[0].Points.Clear();
+            X.Clear();
+            Y.Clear();
+            X2.Clear();
+            Y2.Clear();
+        }
+        int Perevirka(double x, double y, int c, double x2,double x3,double y2,double y3)
+        {
+            List<double> X0 = new List<double>();
+            List<double> Y0 = new List<double>();
+            X0.Add(x2);
+            X0.Add(x3);
+            X0.Sort();
+            Y0.Add(y2);
+            Y0.Add(y3);
+            Y0.Sort();
+            if (Math.Round(x) > Math.Round(X2.Max()) || Math.Round(x) < Math.Round(X2.Min()))
+            {
+                c = 1;
+            }
+            else if (Math.Round(y) > Math.Round(Y2.Max()) || Math.Round(y) < Math.Round(Y2.Min()))
+            {
+                c = 1;
+            }
+            else if(Math.Round(x) > Math.Round(X0[1]) || Math.Round(x) < Math.Round(X0[0]))
+            {
+                c = 1;
+            }
+            else if (Math.Round(y) > Math.Round(Y0[1]) || Math.Round(y) < Math.Round(Y0[0]))
+            {
+                c = 1;
+            }
+            else c = 0;
+            return c;
+        }
+        void Draw()
+        {
+            for (int i = 1; i < chart1.Series.Count; i++)
+            {
+                chart1.Series.RemoveAt(i);
+                i--;
+            }
+            for (int i = 0; i < X.Count; i+=2)
+            {
+                if (X[i] > X2.Max() && X[i+1] > X2.Max())
+                {
+                    X.RemoveAt(i);
+                    X.RemoveAt(i);
+                    Y.RemoveAt(i);
+                    Y.RemoveAt(i);
+                    //chart1.Series.RemoveAt(i / 2 + 1);
+                    i-=2;
+                }
+                else if(X[i] < X2.Min() && X[i + 1] < X2.Min())
+                {
+                    X.RemoveAt(i);
+                    X.RemoveAt(i);
+                    Y.RemoveAt(i);
+                    Y.RemoveAt(i);
+                    //chart1.Series.RemoveAt(i / 2 + 1);
+                    i -= 2;
+                }
+                else if (Y[i] < Y2.Min() && Y[i + 1] < Y2.Min())
+                {
+                    X.RemoveAt(i);
+                    X.RemoveAt(i);
+                    Y.RemoveAt(i);
+                    Y.RemoveAt(i);
+                    //chart1.Series.RemoveAt(i / 2 + 1);
+                    i -= 2;
+                }
+                else if (Y[i] > Y2.Max() && Y[i + 1] > Y2.Max())
+                {
+                    X.RemoveAt(i);
+                    X.RemoveAt(i);
+                    Y.RemoveAt(i);
+                    Y.RemoveAt(i);
+                    //chart1.Series.RemoveAt(i / 2 + 1);
+                    i -= 2;
+                }
+                else if(X[i] <= X2.Max() && X[i + 1] <= X2.Max() && X[i] >= X2.Min() && X[i + 1] >= X2.Min() && Y[i] <= Y2.Max() && Y[i + 1] <= Y2.Max() && Y[i] >= Y2.Min() && Y[i + 1] >= Y2.Min())
+                {
+                    Series s = new Series();
+                    s.ChartType = SeriesChartType.Line;
+                    s.BorderWidth = 4;
+                    s.Points.AddXY(X[i], Y[i]);
+                    s.Points.AddXY(X[i+1], Y[i+1]);
+                    chart1.Series.Add(s);
+                }
+            }
+            if (X.Count != 0)
+            {
+                c = 0;
+                List<double> A2 = new List<double>();
+                List<double> B2 = new List<double>();
+                List<double> C2 = new List<double>();
+                A2.Add(Y2[0] - Y2[1]);
+                A2.Add(Y2[1] - Y2[1]);
+                A2.Add(Y2[1] - Y2[0]);
+                A2.Add(Y2[0] - Y2[0]);
+                B2.Add(X2[0] - X2[0]);
+                B2.Add(X2[0] - X2[1]);
+                B2.Add(X2[1] - X2[1]);
+                B2.Add(X2[1] - X2[0]);
+                C2.Add(X2[0] * Y2[1] - Y2[0] * X2[0]);
+                C2.Add(-X2[0] * Y2[1] + Y2[1] * X2[1]);
+                C2.Add(X2[1] * Y2[0] - Y2[1] * X2[1]);
+                C2.Add(-X2[1] * Y2[0] + Y2[0] * X2[0]);
+                
+                for (int i = 0; i < X.Count(); i += 2)
+                {
+                    List<double> P1 = new List<double>();
+                    List<double> P2 = new List<double>();
+                    List<int> C = new List<int>();
+                    double a1, b1, c1;
+                    double x, y;
+                    a1 = Y[i] - Y[i + 1];
+                    b1 = X[i + 1] - X[i];
+                    c1 = X[i] * Y[i + 1] - Y[i] * X[i + 1];
+                    for (int j = 0; j < 4; j++)
+                    {
+                        x = -(c1 * B2[j] - b1 * C2[j]) / (a1 * B2[j] - b1 * A2[j]);
+                        y = -(a1 * C2[j] - c1 * A2[j]) / (a1 * B2[j] - b1 * A2[j]);
+                        c = Perevirka(x, y, c, X[i], X[i + 1], Y[i], Y[i + 1]);
+                        if (c == 0)
+                        {
+                            P1.Add(x);
+                            P2.Add(y);
+                        }
+                        C.Add(c);
+                    }
+                    if (P1.Count == 2)
+                    {
+                        Series s = new Series();
+                        s.ChartType = SeriesChartType.Line;
+                        s.BorderWidth = 4;
+                        s.Points.AddXY(P1[0], P2[0]);
+                        s.Points.AddXY(P1[1], P2[1]);
+                        chart1.Series.Add(s);
+                    }
+                    else if (P1.Count == 1)
+                    {
+                        if (C[0] == 0)
+                        {
+                            if (X[i] > X[i + 1])
+                            {
+                                Series s1 = new Series();
+                                s1.ChartType = SeriesChartType.Line;
+                                s1.BorderWidth = 4;
+                                s1.Points.AddXY(P1[0], P2[0]);
+                                s1.Points.AddXY(X[i], Y[i]);
+                                chart1.Series.Add(s1);
+                            }
+                            else
+                            {
+                                Series s = new Series();
+                                s.ChartType = SeriesChartType.Line;
+                                s.BorderWidth = 4;
+                                s.Points.AddXY(P1[0], P2[0]);
+                                s.Points.AddXY(X[i + 1], Y[i + 1]);
+                                chart1.Series.Add(s);
+                            }
+                        }
+                        else if (C[1] == 0)
+                        {
+                            if (Y[i] > Y[i + 1])
+                            {
+                                Series s1 = new Series();
+                                s1.ChartType = SeriesChartType.Line;
+                                s1.BorderWidth = 4;
+                                s1.Points.AddXY(P1[0], P2[0]);
+                                s1.Points.AddXY(X[i], Y[i]);
+                                chart1.Series.Add(s1);
+                            }
+                            else
+                            {
+                                Series s = new Series();
+                                s.ChartType = SeriesChartType.Line;
+                                s.BorderWidth = 4;
+                                s.Points.AddXY(P1[0], P2[0]);
+                                s.Points.AddXY(X[i + 1], Y[i + 1]);
+                                chart1.Series.Add(s);
+                            }
+                        }
+                        else if (C[3] == 0)
+                        {
+                            if (Y[i] > Y[i + 1])
+                            {
+                                Series s1 = new Series();
+                                s1.ChartType = SeriesChartType.Line;
+                                s1.BorderWidth = 4;
+                                s1.Points.AddXY(P1[0], P2[0]);
+                                s1.Points.AddXY(X[i+1], Y[i+1]);
+                                chart1.Series.Add(s1);
+                            }
+                            else
+                            {
+                                Series s = new Series();
+                                s.ChartType = SeriesChartType.Line;
+                                s.BorderWidth = 4;
+                                s.Points.AddXY(P1[0], P2[0]);
+                                s.Points.AddXY(X[i], Y[i]);
+                                chart1.Series.Add(s);
+                            }
+                        }
+                        else if (C[2] == 0)
+                        {
+                            if (X[i] > X[i + 1])
+                            {
+                                Series s1 = new Series();
+                                s1.ChartType = SeriesChartType.Line;
+                                s1.BorderWidth = 4;
+                                s1.Points.AddXY(P1[0], P2[0]);
+                                s1.Points.AddXY(X[i+1], Y[i+1]);
+                                chart1.Series.Add(s1);
+                            }
+                            else
+                            {
+                                Series s = new Series();
+                                s.ChartType = SeriesChartType.Line;
+                                s.BorderWidth = 4;
+                                s.Points.AddXY(P1[0], P2[0]);
+                                s.Points.AddXY(X[i], Y[i]);
+                                chart1.Series.Add(s);
+                            }
+                        }
+                    }
+                }
+            }
+            c = 0;
+        }
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            Draw();
+        }
+    }
+}
